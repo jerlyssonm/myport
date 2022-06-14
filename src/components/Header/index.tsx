b3158637136b09b3d0529d7b80c59af220adcebb
Jerlysson <jerlyssonm@gmail.com>
2022-06-13 23:55:11 -0300

    fix: header Done with responsive menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,39 @@
-import { NavBox, Logo, LogoInput, LogoLabel, LogoList, Menu, ListMenu } from "./style"
+/* import { NavBox, Logo, LogoInput, LogoLabel, LogoList, Menu, ListMenu } from "./style" */
 import { GrMenu } from "react-icons/gr"
+import "./style.css"
 
 const Header = () => {
     return (
         <header>
-            <NavBox>
-                <Logo>Dev Jerlysson</Logo>
-                <LogoInput type="checkbox" id="toggler" />
-                <LogoLabel for="toggler"><GrMenu/></LogoLabel>
-                <Menu>
-                    <LogoList>
-                        <li><ListMenu href="">Quem Sou</ListMenu></li>
-                        <li><ListMenu href="">Projetos</ListMenu></li>
-                        <li><ListMenu href="">Serviços</ListMenu></li>
-                        <li><ListMenu href="">Conhecimento</ListMenu></li>
-                    </LogoList>
-                </Menu>
-            </NavBox>
+            <nav className="navbar">
+                <a href="#" className="logo">Dev Jerlysson</a>
+                <input type="checkbox" id="toggler"/>
+                <label htmlFor="toggler"><GrMenu/></label>
+                <div className="menu">
+                    <ul className="list">
+                        <li><a href="#">Quem Sou</a></li>
+                        <li><a href="#">Projetos</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Conhecimentos</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
 
 export default Header
+/* <NavBox>
+<Logo>Dev Jerlysson</Logo>
+<LogoInput type="checkbox" id="toggler"/>
+<LogoLabel htmlFor="toggler"><GrMenu/></LogoLabel>
+<Menu>
+    <LogoList>
+        <li><ListMenu href="">Quem Sou</ListMenu></li>
+        <li><ListMenu href="">Projetos</ListMenu></li>
+        <li><ListMenu href="">Serviços</ListMenu></li>
+        <li><ListMenu href="">Conhecimento</ListMenu></li>
+    </LogoList>
+</Menu>
+</NavBox> 
+*/
